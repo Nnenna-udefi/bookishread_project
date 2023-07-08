a7b6564829e7bf6d2a5ec7e0253bdb98a1d1c719
Nnenna Udefi <miriamudefi@gmail.com>
2023-07-08 20:14:24 +0100

    Update get_book.py

--- web_flask/get_book.py
+++ web_flask/get_book.py
@@ -2,6 +2,9 @@
 import csv
 
 def get_book_by_id(book_id):
+    """ 
+        function gets the id of a specific book based on the id assigned to that book
+    """
     with open('books_data.csv', 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
